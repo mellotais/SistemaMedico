b09a5574306c06c6131f0b98b9c133e2bd5a4870
Taís Mello <105865585+mellotais@users.noreply.github.com>
2024-08-19 17:22:22 -0300

    Update apresentacao.js

--- routes/apresentacao.js
+++ routes/apresentacao.js
@@ -1,5 +1,3 @@
-// routes/doctor.js, routes/patient.js, routes/emergency.js
-
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
@@ -7,7 +5,6 @@ const authMiddleware = require('../middlewares/authMiddleware');
 // Aplicar o middleware de autenticação às rotas protegidas
 router.use(authMiddleware);
 
-// Suas rotas protegidas aqui
 router.get('/', (req, res) => {
   res.render('/apresentacao');
 });
